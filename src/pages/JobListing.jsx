13fb53a33fd27bd1fcceb104bf0ebd09d192dde7
Yash Gupta <yashvinodgupta6@gmail.com>
2026-01-01 20:20:36 +0530

    error

--- src/pages/JobListing.jsx
+++ src/pages/JobListing.jsx
@@ -17,7 +17,7 @@ import {
 } from "@/components/ui/select";
 
 import { getCompanies } from "@/api/apiCompanies";
-import { getJobs } from "@/api/apijobs";
+import { getJobs } from "@/api/apiJobs";
 
 
 const JobListing = () => {
